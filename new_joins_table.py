1dffd6c3f8f3202bded8d2f99d78ac60ff86211f
Kristian Golemi <krigol0014@gmail.com>
2021-02-23 14:54:38 +0200

    added import statements for Table class due to errors

--- new_joins_table.py
+++ new_joins_table.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # left outer join returns the matching rows as well as the rows which are in the left table but not in the right table
 def _left_outer_join(self, table_right: Table, condition):
+        from table import Table
         '''
         Join table (left) with a supplied table (right) where condition is met.
         '''
@@ -58,6 +59,7 @@ def _left_outer_join(self, table_right: Table, condition):
 
 
 def _right_outer_join(self, table_right: Table, condition):
+        from table import Table
         '''
         Join table (left) with a supplied table (right) where condition is met.
         '''
@@ -111,6 +113,7 @@ def _right_outer_join(self, table_right: Table, condition):
 
 
 def _full_outer_join(self, table_right: Table, condition):
+        from table import Table
         '''
         Join table (left) with a supplied table (right) where condition is met.
         '''
@@ -181,6 +184,7 @@ def _full_outer_join(self, table_right: Table, condition):
 
 
 def _inl_join(self, table_right: Table, condition):
+        from table import Table
         from database import Database
         '''
         Join table (left) with a supplied table (right) where condition is met.
@@ -244,6 +248,7 @@ def _inl_join(self, table_right: Table, condition):
     # 2) merge-like concurrently scan the two tables
     # 3) return the join_table containing the matching rows, found through merging the tables
 def _sm_join(self, table_right: Table, condition):
+        from table import Table
         '''
         Join table (left) with a supplied table (right) where condition is met.
         '''
