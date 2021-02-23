1dfd81b08e60ab4ff0923e7ef18c04ebc69c1df9
Kristian Golemi <krigol0014@gmail.com>
2021-02-23 14:42:21 +0200

    trying to fix circular dependency errors

--- new_joins_table.py
+++ new_joins_table.py
@@ -1,6 +1,5 @@
 # this file should be imported by the table.py file, so that its implementations work 
 # import Database and Btree classes so that we can use their methods and attributes
-from database import Database
 from btree import Btree
 import pandas as pd
 
@@ -182,6 +181,7 @@ def _full_outer_join(self, table_right: Table, condition):
 
 
 def _inl_join(self, table_right: Table, condition):
+    from database import Database
         '''
         Join table (left) with a supplied table (right) where condition is met.
         '''
